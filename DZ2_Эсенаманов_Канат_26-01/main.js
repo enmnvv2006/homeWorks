798a7f20cfdd159799c26b822e4c5b0f33d8cd63
enmnvv2006 <133098965+enmnvv2006@users.noreply.github.com>
2023-05-10 12:12:49 +0300

    выполнил дз2

---
+++ DZ2_Эсенаманов_Канат_26-01/main.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+// 1 Задача
+
+// let arr = [10, 20, 30, 50, 235, 3000, 5000439857];
+
+// for (let i = 0; i < arr.length; i++) {
+//   let num = String(arr[i]);
+//   let char = num[0];
+//   if (char == 1 || char == 2 || char == 5) {
+//     console.log(num);
+//   }
+// }
+
+
+// 2 Задача
+
+// for (i=20;i>-1;i--) {
+//     console.log(i);
+// }
+
+
+// 4 Задача
+
+// var start = 10;
+// var result = [];
+// while (start >= 0) {
+//     result.unshift(start--);
+// }
+// console.log(result);
