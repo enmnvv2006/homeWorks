ccad5b3edc3d4e88127ce37b1aee2a1094f9342d
enmnvv2006 <133098965+enmnvv2006@users.noreply.github.com>
2023-12-21 23:33:20 +0600

    сделал 5 дз

--- project_program/js/lesson.js
+++ project_program/js/lesson.js
@@ -61,3 +61,43 @@ const autoSlider = () => {
 }
 
 autoSlider()
+
+// converter
+
+const som = document.querySelector('#som')
+const usd = document.querySelector('#usd')
+const eur = document.querySelector('#eur')
+
+const converter = (element,targetElement,targetElement2,current) => {
+    element.oninput = () => {
+         const xhr = new XMLHttpRequest()
+         xhr.open('GET','../data/converter.json')
+         xhr.setRequestHeader('Content-type','application/json')
+         xhr.send()
+
+         xhr.onload = () => {
+            const data = JSON.parse(xhr.response)
+
+            switch (current) {
+                case 'som':
+                    targetElement.value = (element.value / data.usd).toFixed(2)
+                    targetElement2.value = (element.value / data.eur).toFixed(2)
+                    break
+                case 'usd':
+                    targetElement.value = (element.value * data.usd).toFixed(2)
+                    targetElement2.value = (element.value * data.usd / data.eur).toFixed(2)
+                    break
+                case 'eur':
+                    targetElement.value = (element.value * data.eur).toFixed(2)
+                    targetElement2.value = (element.value * data.eur / data.usd).toFixed(2)
+                    break
+                default:
+                    break
+            }
+         }
+    }
+}
+
+converter(som,usd,eur,'som')
+converter(usd,som,eur,'usd')
+converter(eur,som,usd,'eur')
