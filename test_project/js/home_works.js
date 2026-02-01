f49e97e2e2aeb44e97bc6179514093661201746d
kanat <133098965+enmnvv2006@users.noreply.github.com>
2026-02-01 18:20:18 +0600

    Add files via upload

--- test_project/js/home_works.js
+++ test_project/js/home_works.js
@@ -16,16 +16,66 @@ gmailButton.addEventListener('click', () => {
 
 // move block
 
-const childBlock = document.querySelector(".child_block");
+const parentBlock = document.querySelector('.parent_block')
+const childBlock = document.querySelector('.child_block')
 
-let num = 0
+let positionX = 0
+let positionY = 0
+
+const offsetWidth = parentBlock.clientWidth - childBlock.clientWidth
+const offsetHeight = parentBlock.clientHeight - childBlock.clientHeight
 
 const move = () => {
-  if (num < 448) {
-    num++;
-    childBlock.style.left = `${num}px`;
-    requestAnimationFrame(move);
-  }
-};
+    if (positionX < offsetWidth && positionY === 0) {
+        positionX++
+    } else if (positionX >= offsetWidth && positionY < offsetHeight) {
+        positionY++
+    } else if (positionY >= offsetHeight && positionX > 0) {
+        positionX--
+    } else if (positionX <= 0 && positionY > 0) {
+        positionY--
+    }
 
-move();
+    childBlock.style.left = `${positionX}px`
+    childBlock.style.top = `${positionY}px`
+
+
+    requestAnimationFrame(move)
+}
+
+move()
+
+// timer
+
+const time = document.querySelector("#seconds")
+const startBtn = document.querySelector("#start")
+const stopBtn = document.querySelector("#stop")
+const resetBtn = document.querySelector("#reset")
+
+let timerId = 0
+let running = false
+
+const timer = () => {
+if (!running) {
+  running = true
+
+  timerId = setInterval(() => {
+  time.innerHTML++
+}, 1000)
+}
+}
+
+startBtn.addEventListener("click",() => {
+  timer()
+})
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId)
+  running = false
+})
+
+resetBtn.addEventListener("click", () => {
+    clearInterval(timerId)
+    time.innerHTML = 0
+    running = false
+})
