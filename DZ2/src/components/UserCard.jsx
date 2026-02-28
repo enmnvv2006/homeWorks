26be9bdc64e843b032a1ad9f2bc3242f6c9cb8ff
kanat <133098965+enmnvv2006@users.noreply.github.com>
2026-02-28 14:29:42 +0600

    Add files via upload

---
+++ DZ2/src/components/UserCard.jsx
@@ -0,0 +1,16 @@
+import { Card } from "antd";
+const { Meta } = Card;
+
+export const UserCard = ({ name, age, city, active, img }) => {
+  return (
+    <Card
+      hoverable
+      style={{ width: 240, display: active ? "block" : "none" }}
+      cover={<img draggable={false} alt="example" src={img} />}
+    >
+      <Meta title={name} />
+      <Meta title={age} />
+      <Meta title={city} />
+    </Card>
+  );
+};
