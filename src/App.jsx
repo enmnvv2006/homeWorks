cd5bc0efb109da9faf983ff6ee910e5069e9fdcd
esnmnvv <user@MacBook-Air--esnmnvv.local>
2026-04-26 20:22:23 +0600

    Сделал дз 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Buy from "./components/Buy/Buy";
+import Hello from "./components/Hello/Hello";
+import Index from "./components/Index/index";
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Index />
+      <Buy />
+    </div>
+  );
+}
+
+export default App;
